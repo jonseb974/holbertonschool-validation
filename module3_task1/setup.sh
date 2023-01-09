a6323704c9a2847a622d4387d754cbe5fa0be6ea
jonseb974 <5151@holbertonstudents.com>
2023-01-09 22:17:14 +0100

    update to download parameter

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update && apt-get install -y hugo make
 sudo apt-get install curl -y 
-sudo curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb
+sudo curl -L install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb
 sudo apt-get install ./install_hugo.deb
