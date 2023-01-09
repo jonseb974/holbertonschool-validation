1bbd0a26095f45f28b226975744dcf1b1fbf2335
jonseb974 <5151@holbertonstudents.com>
2023-01-09 22:21:33 +0100

    new acript test trying new parameters

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get update && apt-get install -y hugo make
 sudo apt-get install curl -y 
-sudo curl -L install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb
-sudo apt-get install ./install_hugo.deb
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb -o last.deb
+sudo apt-get install ./last.deb
+sudo rm last.deb
