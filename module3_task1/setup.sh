d73e8a534980a549e51595f7aa22f528daaf40aa
jonseb974 <5151@holbertonstudents.com>
2023-01-09 21:47:58 +0100

    update to setup file format

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update && apt-get install -y hugo make
 apt-get install curl -y 
-curl -L install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb
 apt-get install ./install_hugo.deb
