36f2822054b9441bbd958972a801cec037f51fac
jonseb974 <5151@holbertonstudents.com>
2023-01-10 14:51:19 +0100

    testing setup script debuging test

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -3,4 +3,4 @@ sudo apt-get update && apt-get install -y hugo make
 sudo apt-get install curl -y 
 sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb -o last.deb
 sudo apt-get install ./last.deb
-sudo rm last.deb
+# sudo rm last.deb
