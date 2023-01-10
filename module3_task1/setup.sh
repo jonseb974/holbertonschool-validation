65fef85f40283279b754da5325e34943bfc2956e
jonseb974 <5151@holbertonstudents.com>
2023-01-10 15:48:16 +0100

    script modification with previous parameters

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt-get update && apt-get install -y make golang-go git curl 
-curl -L install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb -o hugoversion.deb
-apt install ./hugoversion.deb
+sudo apt-get update && apt-get install -y hugo make
+sudo apt-get install curl -y 
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb -o last.deb
+sudo apt-get install ./last.deb
 # sudo rm last.deb
