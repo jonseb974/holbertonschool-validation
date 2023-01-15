d48421f98b86550579882a1de99698194ef95f8e
jonseb974 <5151@holbertonstudents.com>
2023-01-15 14:35:11 +0100

    new ripositorty

---
+++ module3_task5/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update && apt-get install -y hugo make
+apt-get install curl -y 
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb -o last.deb
+apt-get install ./last.deb
+npm install -g markdownlint-cli
+# rm last.deb
