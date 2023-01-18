8c5d1a8ef4b3c21d5bdc0606989e670dd5bd11e7
jonseb974 <5151@holbertonstudents.com>
2023-01-18 15:04:52 +0100

    fix syntax error in the setup script

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 rm -rf /usr/local/go
+# curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb
 # apt-get update && apt-get install -y hugo make
 # apt-get install curl -y 
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb -o last.deb
-sudo apt-get install ./instal_hugo.deb
+sudo apt-get install ./install_hugo.deb
 # apt-get install ./last.deb
 # npm install -g markdownlint-cli
 # rm last.deb
