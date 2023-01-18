9f55a989c7186977cf3a80bbf5aa959436cd8332
jonseb974 <5151@holbertonstudents.com>
2023-01-18 14:56:04 +0100

    update the setup script

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 rm -rf /usr/local/go
-apt-get update && apt-get install -y hugo make
-apt-get install curl -y 
+# apt-get update && apt-get install -y hugo make
+# apt-get install curl -y 
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb -o last.deb
-apt-get install ./last.deb
-npm install -g markdownlint-cli
+sudo apt-get install ./instal_hugo.deb
+# apt-get install ./last.deb
+# npm install -g markdownlint-cli
 # rm last.deb
