c7f253124e0563caa31f9c1b0d747c2390f780fa
jonseb974 <5151@holbertonstudents.com>
2023-01-18 14:41:31 +0100

    add setup script update

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#rm -rf /usr/local/go
+rm -rf /usr/local/go
 apt-get update && apt-get install -y hugo make
 apt-get install curl -y 
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-amd64.deb -o last.deb
